fda81a73a85ebe0394fe682fa14dbb67788b2bf6
Ashley King <ashleyot@gmail.com>
2019-05-19 16:41:31 -0400

    finished the recipelist component

---
+++ src/components/Recipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Recipe extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from recipe</h3>
+      </div>
+    );
+  }
+}
---
+++ src/components/Search.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+export default class Search extends Component {
+  render() {
+    const { handleChange, handleSubmit, search } = this.props;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto cold-md-8 mt-5 text-center">
+            <h1 className="text-slanted text-capitalize">
+              search recipes with{" "}
+              <strong className="text-orange">Food2Fork</strong>
+            </h1>
+            <form className="mt-4">
+              <label htmlFor="search" className="text-capitalize">
+                type recipes separated by commas
+              </label>
+              <div className="input-group">
+                <input
+                  type="text"
+                  name="search"
+                  className="form-control"
+                  placeholder="chicken, carrots"
+                  value={search}
+                  onChange={handleChange}
+                />
+                <div className="input-group-append">
+                  <button
+                    type="submit"
+                    className="input-group-text text-white bg-primary"
+                    onClick={handleSubmit}
+                  >
+                    <i className="fas fa-search" />
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
